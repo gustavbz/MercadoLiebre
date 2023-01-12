c2d3e4d4a7c9df8a60b32bbb3e2f6167d839f191
tavo cardenas <bernaldez.raul@gmail.com>
2023-01-11 21:41:13 -0500

    config para deploy

--- app.js
+++ app.js
@@ -5,9 +5,10 @@ const publicPath = path.resolve(__dirname, "./public");
 
 app.use(express.static(publicPath));
 
-app.listen(3000, () =>{
-    console.log("servidor corriendo")
-});
+const port = process.env.PORT || 3000;
+
+app.listen(port, () =>
+    console.log(`servidor corriendo${port}`));
 
 app.get("/home", (req,res)=>{
     res.sendFile(path.resolve(__dirname, "./views/home.html"))
